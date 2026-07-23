3b32e98da25c1a1cee1563e4db49688a984c727a
MD Taizol Islam <119168351+MdTaizol@users.noreply.github.com>
2026-07-22 22:34:27 -1200

    Add files via upload
    c++ all Basic this Folder

---
+++ c++BASIC/pointer.cpp
@@ -0,0 +1,52 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+//  int main()
+//  {
+
+    // address find:
+
+    // int x ;
+    // cout<<&x;
+ 
+ 
+    // // pointers
+    //  int x;
+    //  int *p_x;
+
+    // int a[10];
+    // cout<<a<<endl;
+    // int g;
+    // cout<<&g;
+
+//    //pointer and double pointers :  
+//   int x = 4;
+//   int * p_x = &x;
+//   cout<< "aDDrs x : "<< &x<<endl;
+//   cout<< "aDDRs x : "<< p_x<<endl;
+//   cout<< "value of x : "<<*p_x<<endl;
+//   *p_x=5;
+//   cout<<"x : "<< x <<endl;
+//   //adding or 4 byte and remainder byte adding at the last :
+//   cout<<"p_x +1 : " << p_x+1<<endl; 
+
+// //   double pointers :
+//     int **p_p_x = &p_x;
+//     cout<<" ADDRess p_X: "<<&p_x<<endl;
+    
+//     cout<<" ADDRess p_p_X: "<<p_p_x<<endl;
+//     **p_p_x = 7;
+//     cout<< "X : " <<x <<endl;
+
+//  }
+
+// function in pointers uses:
+// void increment(int *x ){
+//     // (*x)++;
+//     *x = *x + 2;
+// }
+// int main(){
+//     int a = 4;
+//     cout<<a<<endl;
+//     increment(&a);
+//     cout<<a <<endl;
+// }
