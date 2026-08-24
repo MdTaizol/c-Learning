f15ebe788d4d0d38478e7247fbaa44a32e10962b
MD Taizol Islam <119168351+MdTaizol@users.noreply.github.com>
2026-08-24 23:40:23 +0600

    Add files via upload

---
+++ pattern/pattern2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void prin(int n){
+    for(int i = 0 ; i< n; i++){
+        for(int j = 0; j<=i ; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        prin(n);
+    }
+}
+
+
+//  * 
+//  *  * 
+//  *  *  * 
+//  *  *  *  * 
+//  * 
+//  *  * 
+//  *  *  * 
+//  *  *  *  * 
+//  *  *  *  *  *
